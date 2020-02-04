f350ac0d732d741f7362aaa3460f52627ad70e60
TheOrangeKnight <fenixdd86@gmail.com>
2020-02-04 19:32:10 +0100

    Changes in pom file to include tracer.jar and the use to trace the exceptions

--- src/main/java/com/iteriam/calculator/controller/ExceptionController.java
+++ src/main/java/com/iteriam/calculator/controller/ExceptionController.java
@@ -2,6 +2,7 @@ package com.iteriam.calculator.controller;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,28 +12,32 @@ import com.iteriam.calculator.enums.ErrorCode;
 import com.iteriam.calculator.exception.StandardException;
 import com.iteriam.calculator.response.ErrorResponse;
 
+import io.corp.calculator.TracerImpl;
+
 @ControllerAdvice
 public class ExceptionController {
 
+	 TracerImpl trace = new TracerImpl();
+	
 	@ExceptionHandler(StandardException.class)
-	public ResponseEntity<ErrorResponse> standardExceptionHandler(StandardException e, WebRequest webReq) {
+	public ResponseEntity<ErrorResponse> standardExceptionHandler(StandardException ex, WebRequest webReq) {
 
-		ErrorResponse errRes = new ErrorResponse(e.getError().getCode(), e.getError().getMessage(),
-				e.getError().getHttpStatus(), new Date(), webReq.getDescription(true));
+		ErrorResponse errRes = new ErrorResponse(ex.getError().getCode(), ex.getError().getMessage(),
+				ex.getError().getHttpStatus(), new Date(), webReq.getDescription(true));
 
-		// TODO SET ERROR TRACE HERE
+		trace.trace(ex.getError().getMessage());
 
-		return new ResponseEntity<>(errRes, e.getError().getHttpStatus());
+		return new ResponseEntity<>(errRes, ex.getError().getHttpStatus());
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponse> exceptionHandler(WebRequest webReq) {
+	public ResponseEntity<ErrorResponse> exceptionHandler(Exception ex, WebRequest webReq) {
 
 		ErrorResponse errRes = new ErrorResponse(ErrorCode.GENERIC_EXCEPTION.getCode(),
 				ErrorCode.GENERIC_EXCEPTION.getMessage(), ErrorCode.GENERIC_EXCEPTION.getHttpStatus(), new Date(),
 				webReq.getDescription(true));
 
-		// TODO SET ERROR TRACE HERE
+		trace.trace(ex.getMessage());
 
 		return new ResponseEntity<>(errRes, ErrorCode.GENERIC_EXCEPTION.getHttpStatus());
 	}
